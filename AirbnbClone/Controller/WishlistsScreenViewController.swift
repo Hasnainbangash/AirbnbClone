ceda713e653684fcc595a42ab509f156ae35e4a7
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-07 16:48:27 +0500

    Wish liMain wishlist screen work done

--- AirbnbClone/Controller/WishlistsScreenViewController.swift
+++ AirbnbClone/Controller/WishlistsScreenViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class WishlistsScreenViewController: UIViewController {
 
+    @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var allImagesView: UIView!
     
     override func viewDidLoad() {
@@ -19,6 +20,7 @@ class WishlistsScreenViewController: UIViewController {
         setupCornerRadius()
         setupBorderWidth()
         setupBorderShadow()
+        setupButtonUnderline()
         
     }
     
@@ -42,6 +44,22 @@ class WishlistsScreenViewController: UIViewController {
         // Make the shadow more "realistic" by enabling shadow path
         allImagesView.layer.masksToBounds = false // Important for shadow to appear outside bounds
         allImagesView.layer.shadowPath = UIBezierPath(rect: allImagesView.bounds).cgPath
+    }
+    
+    func setupButtonUnderline() {
+        guard let buttonText = editButton.titleLabel?.text else {return}
+        
+        // Create an NSMutableAttributedString from the button's title text
+        let attributedString = NSMutableAttributedString(string: buttonText)
+        
+        // Add underline attribute to the full text
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: buttonText.count))
+        
+        // Set the attributed text to the button's title
+        editButton.setAttributedTitle(attributedString, for: .normal)
+        
+        // Optional: Change the text color (you can set any color you prefer)
+        editButton.setTitleColor(UIColor.blue, for: .normal)
     }
     
 }
