457ba443a64695a340ff52d4738b7af1f3380c56
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-07 16:22:10 +0500

    Make the border radius work done in th wish list screen for the view that contians all images

--- AirbnbClone/Controller/WishlistsScreenViewController.swift
+++ AirbnbClone/Controller/WishlistsScreenViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class WishlistsScreenViewController: UIViewController {
 
+    @IBOutlet weak var allImagesView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        setupCornerRadius()
+        setupBorderWidth()
+        setupBorderShadow()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupCornerRadius() {
+        allImagesView.layer.cornerRadius = allImagesView.frame.height / 10
     }
-    */
-
+    
+    func setupBorderWidth() {
+        // Border settings
+        allImagesView.layer.borderWidth = 5.0
+        allImagesView.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func setupBorderShadow() {
+        // Shadow settings
+        allImagesView.layer.shadowColor = UIColor.black.cgColor  // Shadow color
+        allImagesView.layer.shadowOpacity = 0.3                 // Shadow opacity (0.0 to 1.0)
+        allImagesView.layer.shadowOffset = CGSize(width: 0, height: 3) // Shadow offset (you can change this)
+        allImagesView.layer.shadowRadius = 5                     // Shadow blur radius (the higher the more blur)
+        
+        // Make the shadow more "realistic" by enabling shadow path
+        allImagesView.layer.masksToBounds = false // Important for shadow to appear outside bounds
+        allImagesView.layer.shadowPath = UIBezierPath(rect: allImagesView.bounds).cgPath
+    }
+    
 }
