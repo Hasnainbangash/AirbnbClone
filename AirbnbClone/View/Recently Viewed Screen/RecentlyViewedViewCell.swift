60cf51e8b1082635beb76609c5af5bb3464af640
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-07 17:23:43 +0500

    Recently view cell customclass code work done

--- AirbnbClone/View/Recently Viewed Screen/RecentlyViewedViewCell.swift
+++ AirbnbClone/View/Recently Viewed Screen/RecentlyViewedViewCell.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class RecentlyViewedViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var placeName: UILabel!
+    @IBOutlet weak var bedsLabel: UILabel!
+    @IBOutlet weak var bedroomsLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func setupCornerRadius() {
+        imageView.layer.cornerRadius = imageView.frame.height / 5
+    }
+    
+    func configureCell(placeImage: String?, placeName: String, totalBeds: String, totalbedRooms: String) {
+        
+//        self.imageView.image = UIImage(named: placeName)
+        self.placeName.text = placeName
+        self.bedsLabel.text = totalBeds + " beds ."
+        self.bedroomsLabel.text = totalbedRooms + " bedrooms"
+        
     }
 
 }
