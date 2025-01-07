fb19e713dd6e09a5a8591c4e92b800fa6f9a04f1
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-07 17:15:45 +0500

    Recently viewed collection cell work done

---
+++ AirbnbClone/View/Recently Viewed Screen/RecentlyViewedViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecentlyViewedViewCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 07/01/2025.
+//
+
+import UIKit
+
+class RecentlyViewedViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
