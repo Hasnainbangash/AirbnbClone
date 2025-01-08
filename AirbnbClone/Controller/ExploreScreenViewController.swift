bc74dea89f3952815d9382ca4c64d3f448259f04
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-08 17:50:08 +0500

    Data added in the catergory collection with name and images

--- AirbnbClone/Controller/ExploreScreenViewController.swift
+++ AirbnbClone/Controller/ExploreScreenViewController.swift
@@ -15,7 +15,68 @@ class ExploreScreenViewController: UIViewController {
     
     var exploreCategories: [ExploreCategories] = [
         
-        ExploreCategories(categoryName: "Amazing Views", categoryImage: "window.awning")
+        ExploreCategories(categoryName: "Lake", categoryImage: "water.waves"),
+        ExploreCategories(categoryName: "Icons", categoryImage: "line.horizontal.star.fill.line.horizontal"),
+        ExploreCategories(categoryName: "Arctic", categoryImage: "snowflake"),
+        ExploreCategories(categoryName: "Islands", categoryImage: "sun.haze"),
+        ExploreCategories(categoryName: "Top cities", categoryImage: "building.2"),
+        ExploreCategories(categoryName: "Design", categoryImage: "house.and.flag"),
+        ExploreCategories(categoryName: "Rooms", categoryImage: "bed.double"),
+        ExploreCategories(categoryName: "Amazing Views", categoryImage: "window.casement"),
+        ExploreCategories(categoryName: "Mansions", categoryImage: "building.columns"),
+        ExploreCategories(categoryName: "Tropical", categoryImage: "tree"),
+        ExploreCategories(categoryName: "Tiny homes", categoryImage: "house"),
+        ExploreCategories(categoryName: "OMG!", categoryImage: "manatsign.bank.building"),
+        ExploreCategories(categoryName: "Trending", categoryImage: "flame"),
+        ExploreCategories(categoryName: "Earth homes", categoryImage: "house"),
+        ExploreCategories(categoryName: "Castles", categoryImage: "building.columns"),
+        ExploreCategories(categoryName: "Bed & breakfasts", categoryImage: "cup.and.heat.waves"),
+        ExploreCategories(categoryName: "Camping", categoryImage: "tent"),
+        ExploreCategories(categoryName: "Historical homes", categoryImage: "building.columns"),
+        ExploreCategories(categoryName: "Cabins", categoryImage: "house"),
+        ExploreCategories(categoryName: "A-frames", categoryImage: "tent.2"),
+        ExploreCategories(categoryName: "Countryside", categoryImage: "cloud.sun"),
+        ExploreCategories(categoryName: "Luxe", categoryImage: "bell.circle"),
+        ExploreCategories(categoryName: "Houseboats", categoryImage: "house"),
+        ExploreCategories(categoryName: "Riads", categoryImage: "building.columns"),
+        ExploreCategories(categoryName: "Containers", categoryImage: "square.grid.3x1.below.line.grid.1x2"),
+        ExploreCategories(categoryName: "Barns", categoryImage: "building.columns"),
+        ExploreCategories(categoryName: "Top of ther world", categoryImage: "mountain.2"),
+        ExploreCategories(categoryName: "National parks", categoryImage: "tree"),
+        ExploreCategories(categoryName: "Caves", categoryImage: "mountain.2.circle"),
+        ExploreCategories(categoryName: "Amazing pools", categoryImage: "water.waves"),
+        ExploreCategories(categoryName: "New", categoryImage: "key.horizontal"),
+        ExploreCategories(categoryName: "Farms", categoryImage: "house.lodge"),
+        ExploreCategories(categoryName: "Off-the-grid", categoryImage: "square.grid.3x3"),
+        ExploreCategories(categoryName: "Cycladic homes", categoryImage: "house"),
+        ExploreCategories(categoryName: "Dammusi", categoryImage: "homekit"),
+        ExploreCategories(categoryName: "Surfing", categoryImage: "sailboat"),
+        ExploreCategories(categoryName: "Yurts", categoryImage: "tent.2"),
+        ExploreCategories(categoryName: "Treehouses", categoryImage: "tree"),
+        ExploreCategories(categoryName: "Beachfront", categoryImage: "sun.dust"),
+        ExploreCategories(categoryName: "Beach", categoryImage: "beach.umbrella"),
+        ExploreCategories(categoryName: "Camper vans", categoryImage: "bus.fill"),
+        ExploreCategories(categoryName: "Lakefront", categoryImage: "thermometer.and.liquid.waves.snowflake"),
+        ExploreCategories(categoryName: "Golfing", categoryImage: "figure.golf"),
+        ExploreCategories(categoryName: "Skiing", categoryImage: "line.diagonal"),
+        ExploreCategories(categoryName: "Hanoks", categoryImage: "house"),
+        ExploreCategories(categoryName: "Vineyards", categoryImage: "tent"),
+        ExploreCategories(categoryName: "Chef's kitchen", categoryImage: "hat.cap"),
+        ExploreCategories(categoryName: "Windmills", categoryImage: "millsign.bank.building"),
+        ExploreCategories(categoryName: "Shepherd's huts", categoryImage: "house"),
+        ExploreCategories(categoryName: "Ryokans", categoryImage: "tent"),
+        ExploreCategories(categoryName: "Minsus", categoryImage: "house.lodge"),
+        ExploreCategories(categoryName: "Domes", categoryImage: "tent.2"),
+        ExploreCategories(categoryName: "Desert", categoryImage: "tree"),
+        ExploreCategories(categoryName: "Casas particulares", categoryImage: "window.awning"),
+        ExploreCategories(categoryName: "Towers", categoryImage: "airport.extreme.tower"),
+        ExploreCategories(categoryName: "Adapted", categoryImage: "house.lodge"),
+        ExploreCategories(categoryName: "Ski-in/out", categoryImage: "waveform.and.person.filled"),
+        ExploreCategories(categoryName: "Play", categoryImage: "figure.cricket"),
+        ExploreCategories(categoryName: "Boats", categoryImage: "sailboat"),
+        ExploreCategories(categoryName: "Creative spaces", categoryImage: "paintpalette"),
+        ExploreCategories(categoryName: "Grand pianos", categoryImage: "pianokeys"),
+        ExploreCategories(categoryName: "Trulli", categoryImage: "house")
         
     ]
     
@@ -60,5 +121,9 @@ extension ExploreScreenViewController: UICollectionViewDataSource {
 }
 
 extension ExploreScreenViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
     
 }
