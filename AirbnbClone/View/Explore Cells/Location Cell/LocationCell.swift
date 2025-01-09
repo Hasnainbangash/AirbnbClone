69d82b13a4c3ba902f5357e49dbfcd9916255429
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-09 12:19:47 +0500

    Location cell completed without constraints

---
+++ AirbnbClone/View/Explore Cells/Location Cell/LocationCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 09/01/2025.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
