9826970c7b57fc83487fdb6605184abe83c46cde
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-10 15:31:26 +0500

    Two more files are added location images cell

---
+++ AirbnbClone/View/Explore Cells/Expore Location Details Cells/Location Pictures Collection Cell/LocationImagesCell.swift
@@ -0,0 +1,17 @@
+//
+//  LocationImagesCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 10/01/2025.
+//
+
+import UIKit
+
+class LocationImagesCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
