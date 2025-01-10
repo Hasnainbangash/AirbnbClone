c90036d33310e11d6dd35b38c715e9b01b0d6b51
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-10 14:29:28 +0500

    Bed cell work done review Cell is added now

---
+++ AirbnbClone/View/Explore Cells/Expore Location Details Cells/Review Collection Cells/ReviewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 10/01/2025.
+//
+
+import UIKit
+
+class ReviewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
