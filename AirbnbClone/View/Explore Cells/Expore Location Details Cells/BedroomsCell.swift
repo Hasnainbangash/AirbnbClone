8b2f68a89f60e66d699effb791570c9f55d2d605
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-10 12:46:24 +0500

    New collection Cell for bedroom is added

---
+++ AirbnbClone/View/Explore Cells/Expore Location Details Cells/BedroomsCell.swift
@@ -0,0 +1,17 @@
+//
+//  BedroomsCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 10/01/2025.
+//
+
+import UIKit
+
+class BedroomsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
