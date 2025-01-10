c9a1db428c8c3484c4d67b43e8c0fbd06b33bb55
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-10 15:07:40 +0500

    Review cell worked deone

--- AirbnbClone/View/Explore Cells/Expore Location Details Cells/Review Collection Cells/ReviewCell.swift
+++ AirbnbClone/View/Explore Cells/Expore Location Details Cells/Review Collection Cells/ReviewCell.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ReviewCell: UICollectionViewCell {
 
+    @IBOutlet weak var noOfMonthsAgo: UILabel!
+    @IBOutlet weak var reviewDescription: UILabel!
+    @IBOutlet weak var showmoreLabel: UILabel!
+    @IBOutlet weak var reviewerImage: UIImageView!
+    @IBOutlet weak var reviewerName: UILabel!
+    @IBOutlet weak var reviewerTimeoOnAirbnb: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configureData(noOfMonthsAgo: String, reviewDescription: String,
+                       reviewerImage: String, reviewerName: String, reviewerTimeOnAirbnb: String) {
+        
+        self.noOfMonthsAgo.text = noOfMonthsAgo
+        self.reviewDescription.text = reviewDescription
+        self.reviewerImage.image = UIImage(named: reviewerImage)
+        self.reviewerName.text = reviewerName
+        self.reviewerTimeoOnAirbnb.text = reviewerTimeOnAirbnb
+        
+        
     }
 
 }
