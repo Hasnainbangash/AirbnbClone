516998f34428f6a633b48908cb7920846e20936a
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-13 17:37:24 +0500

    List from space buttons corner radius work done

--- AirbnbClone/Controller/ListfromSpaceMainViewController.swift
+++ AirbnbClone/Controller/ListfromSpaceMainViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class ListfromSpaceMainViewController: UIViewController {
 
+    @IBOutlet weak var getStartedButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupCornerRadius()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupCornerRadius() {
+        getStartedButtonLabel.layer.cornerRadius = getStartedButtonLabel.frame.height / 5
     }
-    */
 
 }
