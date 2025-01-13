f03203420ea368a815c7820ee71752d89f5f90ba
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-13 14:58:43 +0500

    SiSome colors changing are done in the login and security screen

--- AirbnbClone/Controller/LoginandSecurityViewController.swift
+++ AirbnbClone/Controller/LoginandSecurityViewController.swift
@@ -9,21 +9,31 @@ import UIKit
 
 class LoginandSecurityViewController: UIViewController {
 
+    @IBOutlet weak var learnAboutSafetyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        updateSafetyTipsLabel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateSafetyTipsLabel() {
+        // Full text in the label
+        let fullText = "Learn about safety tips for guests and hosts."
+        
+        // Create a mutable attributed string
+        let attributedString = NSMutableAttributedString(string: fullText)
+        
+        // Define the color rgba(0, 132, 137, 1)
+        let customColor = UIColor(red: 0/255.0, green: 132/255.0, blue: 137/255.0, alpha: 1.0)
+        
+        // Apply color to "guests" and "hosts" words
+        attributedString.addAttribute(.foregroundColor, value: customColor, range: (fullText as NSString).range(of: "guests"))
+        attributedString.addAttribute(.foregroundColor, value: customColor, range: (fullText as NSString).range(of: "hosts"))
+        
+        // Set the styled text to the label
+        learnAboutSafetyLabel.attributedText = attributedString
     }
-    */
-
+    
 }
