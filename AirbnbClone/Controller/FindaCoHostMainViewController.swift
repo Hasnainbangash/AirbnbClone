e79e3a04285e67269a76e6039dbd2578d0040004
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-14 12:01:52 +0500

    Find co host view controller custom class work is done and it's design is done

---
+++ AirbnbClone/Controller/FindaCoHostMainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  FindaCoHostMainViewController.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 14/01/2025.
+//
+
+import UIKit
+
+class FindaCoHostMainViewController: UIViewController {
+
+    @IBOutlet weak var searchView: UIView!
+    @IBOutlet weak var locationButtonView: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupCornerRadius()
+        setupBorderRadius()
+        setupBorderShadow()
+    }
+    
+    func setupCornerRadius() {
+        searchView.layer.cornerRadius = searchView.frame.height / 2
+        locationButtonView.layer.cornerRadius = locationButtonView.frame.height / 4
+    }
+    
+    func setupBorderRadius() {
+        searchView.layer.borderWidth = 0.05
+        searchView.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func setupBorderShadow() {
+        // Shadow settings
+        searchView.layer.shadowColor = UIColor.black.cgColor  // Shadow color
+        searchView.layer.shadowOpacity = 0.05 // Shadow opacity (0.0 to 1.0)
+        searchView.layer.shadowOffset = CGSize(width: 0, height: 5) // Shadow offset (you can change this)
+        searchView.layer.shadowRadius = 2 // Shadow blur radius (the higher the more blur)
+        
+        // Make the shadow more "realistic" by enabling shadow path
+        searchView.layer.masksToBounds = false // Important for shadow to appear outside bounds
+        searchView.layer.shadowPath = UIBezierPath(rect: searchView.bounds).cgPath
+    }
+
+}
