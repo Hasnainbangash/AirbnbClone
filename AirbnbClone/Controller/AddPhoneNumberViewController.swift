071738dff66ddccc0ca3eaa747074b18fd77eac4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-15 16:21:59 +0500

    Some functions added

---
+++ AirbnbClone/Controller/AddPhoneNumberViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddPhoneNumberViewController.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 15/01/2025.
+//
+
+import UIKit
+
+class AddPhoneNumberViewController: UIViewController {
+
+    @IBOutlet weak var phoneNumberView: UIView!
+    @IBOutlet weak var varifyButtonLabel: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupCornerRadius()
+        setupBorderWidth()
+    }
+    
+    func setupCornerRadius() {
+        phoneNumberView.layer.cornerRadius = phoneNumberView.frame.height / 9
+        varifyButtonLabel.layer.cornerRadius = varifyButtonLabel.frame.height / 5
+        
+    }
+    
+    func setupBorderWidth() {
+        phoneNumberView.layer.borderWidth = 0.7
+    }
+
+}
