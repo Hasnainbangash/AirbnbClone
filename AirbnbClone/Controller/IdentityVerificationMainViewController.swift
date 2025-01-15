bb4468e2fb3ce9483a887477462ce0b56489af9e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-15 17:22:24 +0500

    Corner radius code is added and some more work is done in the main stacks are set

--- AirbnbClone/Controller/IdentityVerificationMainViewController.swift
+++ AirbnbClone/Controller/IdentityVerificationMainViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class IdentityVerificationMainViewController: UIViewController {
 
+    @IBOutlet weak var privacyPolicyTextView: UIView!
+    @IBOutlet weak var addanIDButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupCornerradius()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupCornerradius() {
+        privacyPolicyTextView.layer.cornerRadius = privacyPolicyTextView.frame.height / 5
+        addanIDButtonLabel.layer.cornerRadius = addanIDButtonLabel.frame.height / 5
     }
-    */
 
 }
