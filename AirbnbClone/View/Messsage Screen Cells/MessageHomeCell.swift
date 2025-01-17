dabcddaa585b6f7334d752989e134df9000faf29
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-17 16:59:21 +0500

    Message Home cell is added for messages screen just added not made now

---
+++ AirbnbClone/View/Messsage Screen Cells/MessageHomeCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageHomeCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 17/01/2025.
+//
+
+import UIKit
+
+class MessageHomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
