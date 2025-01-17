03cce00423f2aea87f65dce9b62396c0d75f3d8d
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-17 17:21:42 +0500

    Message gome cell code work done

--- AirbnbClone/View/Messsage Screen Cells/Message Home Cell/MessageHomeCell.swift
+++ AirbnbClone/View/Messsage Screen Cells/Message Home Cell/MessageHomeCell.swift
@@ -9,15 +9,36 @@ import UIKit
 
 class MessageHomeCell: UITableViewCell {
 
+    @IBOutlet weak var receiverImageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var timeDay: UILabel!
+    @IBOutlet weak var lastMessage: UILabel!
+    @IBOutlet weak var messageStatus: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setupCornerRadius()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setupCornerRadius() {
+        receiverImageView.layer.cornerRadius = receiverImageView.frame.height / 2
+    }
+    
+    func configureCell(receiverImage: String, name: String, timeDay: String, lastMessage: String, messageStatus: String) {
+        
+        self.receiverImageView.image = UIImage(named: receiverImage)
+        self.name.text = name
+        self.timeDay.text = timeDay
+        self.lastMessage.text = "Airbnb: This case is closed. Still new the update"
+        self.messageStatus.text = messageStatus
+        
     }
     
 }
