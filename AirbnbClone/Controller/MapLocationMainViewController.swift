3a162a39210a3747f9536fc527ad5e4c14fe3702
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-21 12:24:15 +0500

    Custom file for the map location get added and now se to the screen in the view controller

---
+++ AirbnbClone/Controller/MapLocationMainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapLocationMainViewController.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 21/01/2025.
+//
+
+import UIKit
+
+class MapLocationMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
