26cb4af1eb85aef87511f8126c95f41ba3a334e8
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-21 12:29:25 +0500

    Some Iboutlets are done for maplocationmain view controller

--- AirbnbClone/Controller/MapLocationMainViewController.swift
+++ AirbnbClone/Controller/MapLocationMainViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class MapLocationMainViewController: UIViewController {
 
+    @IBOutlet weak var placeName: UILabel!
+    @IBOutlet weak var availableDates: UILabel!
+    @IBOutlet weak var placeHostedName: UILabel!
+    @IBOutlet weak var placePrice: UILabel!
+    @IBOutlet weak var dayNightLabel: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
