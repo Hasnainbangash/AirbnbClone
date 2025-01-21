6a3de043aeb2610feeda912743a891cd19ba9048
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-21 12:59:45 +0500

    Some more code is added in the map location main view controller for the collection view work is done and the page control work is done

--- AirbnbClone/Controller/MapLocationMainViewController.swift
+++ AirbnbClone/Controller/MapLocationMainViewController.swift
@@ -25,6 +25,93 @@ class MapLocationMainViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.isPagingEnabled = true
+        
+        // Setup page control
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.hidesForSinglePage = true
+        
+        collectionView.register(UINib(nibName: K.MapScreensCell.NibNames.mapLocationCellNibName, bundle: nil), forCellWithReuseIdentifier: K.MapScreensCell.Identifiers.mapLocationCellIdentifier)
+        
+        
+        setupCornerRadius()
+        
+    }
+    
+    func setupCornerRadius() {
+        collectionView.layer.cornerRadius = collectionView.frame.height / 17
+    }
+    
+    func configureCell(placeName: String, placeHosterName: String, availableDates: [String], priceOfPlace: String, dayTime: String, rating: String) {
+        
+        self.placeName.text = placeName
+        self.placeHostedName.text = "Hosted by \(placeHosterName)"
+        self.availableDates.text = "\(availableDates[0]) - \(availableDates[1])"
+        self.placePrice.text = priceOfPlace
+        self.dayNightLabel.text = dayTime
+        self.rating.text = rating
+        
     }
 
+}
+
+extension MapLocationMainViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = round(scrollView.contentOffset.x / scrollView.bounds.width)
+        pageControl.currentPage = Int(page)
+    }
+}
+
+extension MapLocationMainViewController {
+    func setupPageControl() {
+        pageControl.addTarget(self, action: #selector(pageControlValueChanged(_:)), for: .valueChanged)
+    }
+    
+    @objc private func pageControlValueChanged(_ sender: UIPageControl) {
+        let page = CGFloat(sender.currentPage)
+        let offset = page * collectionView.bounds.width
+        collectionView.setContentOffset(CGPoint(x: offset, y: 0), animated: true)
+    }
+}
+
+extension MapLocationMainViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let image = images[indexPath.row]
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.MapScreensCell.Identifiers.mapLocationCellIdentifier, for: indexPath) as? MapLocationCell
+        
+        cell?.configureData(imageName: image)
+        
+        return cell ?? UICollectionViewCell()
+        
+    }
+    
+    
+}
+
+extension MapLocationMainViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemPerRow: CGFloat = UIDevice.current.userInterfaceIdiom == .pad ? 1 : 1
+        let padding: CGFloat = 10
+        let totalPadding = padding * (itemPerRow - 1)
+        let availableWidth = collectionView.frame.width - totalPadding
+        let itemWidth = availableWidth / itemPerRow
+
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
 }
