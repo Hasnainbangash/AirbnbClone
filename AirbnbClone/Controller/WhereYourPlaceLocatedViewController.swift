7423ee2e05b6b14e72f80e46dcbe8ecb459166f5
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-23 17:07:07 +0500

    Design work of where your place is alomost done

--- AirbnbClone/Controller/WhereYourPlaceLocatedViewController.swift
+++ AirbnbClone/Controller/WhereYourPlaceLocatedViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class WhereYourPlaceLocatedViewController: UIViewController {
 
+    @IBOutlet weak var saveAndExitButtonLabel: UIButton!
+    @IBOutlet weak var questionsButtonLabel: UIButton!
+    @IBOutlet weak var backButtonLabel: UIButton!
+    @IBOutlet weak var nextButtonLabel: UIButton!
+    
+    @IBOutlet weak var enterYourAddressView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        setupCornerRadius()
+        setupBorderWidth()
+        setupButtonUnderline()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupCornerRadius() {
+        saveAndExitButtonLabel.layer.cornerRadius = saveAndExitButtonLabel.frame.height / 2
+        questionsButtonLabel.layer.cornerRadius = questionsButtonLabel.frame.height / 2
+        nextButtonLabel.layer.cornerRadius = nextButtonLabel.frame.height / 5
+        
+        enterYourAddressView.layer.cornerRadius = enterYourAddressView.frame.height / 2
     }
-    */
+    
+    func setupBorderWidth() {
+        saveAndExitButtonLabel.layer.borderWidth = 0.3
+        questionsButtonLabel.layer.borderWidth = 0.3
+    }
+    
+    func setupButtonUnderline() {
+        guard let buttonText = backButtonLabel.titleLabel?.text else {return}
+        
+        // Create an NSMutableAttributedString from the button's title text
+        let attributedString = NSMutableAttributedString(string: buttonText)
+        
+        // Add underline attribute to the full text
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: buttonText.count))
+        
+        // Set the attributed text to the button's title
+        backButtonLabel.setAttributedTitle(attributedString, for: .normal)
+        
+        // Optional: Change the text color (you can set any color you prefer)
+        backButtonLabel.setTitleColor(UIColor.black, for: .normal)
+    }
 
 }
