d1de57e11e2d42cfbf3c6340aaf0f30d702e10b1
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-23 12:26:59 +0500

    Describe your place collection cell code work done

--- AirbnbClone/View/Host your place screen cells/Which of thse describe your place screen cells/Describe your place Collection Cells/DescribeYourPlaceCollectionCell.swift
+++ AirbnbClone/View/Host your place screen cells/Which of thse describe your place screen cells/Describe your place Collection Cells/DescribeYourPlaceCollectionCell.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class DescribeYourPlaceCollectionCell: UICollectionViewCell {
 
+    @IBOutlet weak var placeImageView: UIImageView!
+    @IBOutlet weak var placeName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func configureCell(placeImageName: String, placeName: String) {
+        
+        self.placeImageView.image = UIImage(systemName: placeImageName)
+        self.placeName.text = placeName
+        
+    }
+    
 }
