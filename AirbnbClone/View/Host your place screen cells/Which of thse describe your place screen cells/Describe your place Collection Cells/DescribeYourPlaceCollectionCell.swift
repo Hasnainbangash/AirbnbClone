d246c6ff3577f50445f518a1722faa883566ff4a
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-23 12:17:21 +0500

    Cells for describe your screne work done

---
+++ AirbnbClone/View/Host your place screen cells/Which of thse describe your place screen cells/Describe your place Collection Cells/DescribeYourPlaceCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  DescribeYourPlaceCollectionCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 23/01/2025.
+//
+
+import UIKit
+
+class DescribeYourPlaceCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ AirbnbClone/View/Host your place screen cells/Which of thse describe your place screen cells/Describe your place header place/DescribeYourPlaceHeaderCell.swift
@@ -0,0 +1,17 @@
+//
+//  DescribeYourPlaceHeaderCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 23/01/2025.
+//
+
+import UIKit
+
+class DescribeYourPlaceHeaderCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
