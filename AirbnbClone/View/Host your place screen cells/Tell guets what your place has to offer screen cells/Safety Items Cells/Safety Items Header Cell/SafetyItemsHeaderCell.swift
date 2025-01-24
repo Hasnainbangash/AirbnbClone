f5c18944c107bc3ef58955537f9b338f93108f21
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-24 12:46:42 +0500

    Header cell for safety items and standout amenitite are done

---
+++ AirbnbClone/View/Host your place screen cells/Tell guets what your place has to offer screen cells/Safety Items Cells/Safety Items Header Cell/SafetyItemsHeaderCell.swift
@@ -0,0 +1,17 @@
+//
+//  SafetyItemsHeaderCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 24/01/2025.
+//
+
+import UIKit
+
+class SafetyItemsHeaderCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ AirbnbClone/View/Host your place screen cells/Tell guets what your place has to offer screen cells/Standout Amenities Cells/Standout Amenitites Header Cell/StandoutAmenitiesHeaderCell.swift
@@ -0,0 +1,17 @@
+//
+//  StandoutAmenitiesHeaderCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 24/01/2025.
+//
+
+import UIKit
+
+class StandoutAmenitiesHeaderCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
