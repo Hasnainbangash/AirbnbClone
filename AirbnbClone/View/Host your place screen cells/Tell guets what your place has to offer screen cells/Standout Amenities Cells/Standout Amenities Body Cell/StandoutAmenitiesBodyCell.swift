43824496215953be831da45f0f34fdb1ecb63aa8
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-24 12:58:04 +0500

    Standout Amenitites Body Cell work done

---
+++ AirbnbClone/View/Host your place screen cells/Tell guets what your place has to offer screen cells/Standout Amenities Cells/Standout Amenities Body Cell/StandoutAmenitiesBodyCell.swift
@@ -0,0 +1,38 @@
+//
+//  StandoutAmenitiesBodyCell.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 24/01/2025.
+//
+
+import UIKit
+
+class StandoutAmenitiesBodyCell: UICollectionViewCell {
+
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var placeImageView: UIImageView!
+    @IBOutlet weak var placeName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        setupCornerRadius()
+        setupBorderWidth()
+    }
+    
+    func setupCornerRadius() {
+        mainView.layer.cornerRadius = mainView.frame.height / 7
+    }
+    
+    func setupBorderWidth() {
+        mainView.layer.borderWidth = 0.4
+    }
+
+    func configureCell(placeImageName: String, placeName: String) {
+        
+        self.placeImageView.image = UIImage(systemName: placeImageName)
+        self.placeName.text = placeName
+        
+    }
+
+}
