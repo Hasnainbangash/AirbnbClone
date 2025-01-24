e1063f4baa32bd949e6364b41a30ad43d7902900
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-24 12:04:27 +0500

    Make your place satnd out work done

---
+++ AirbnbClone/Controller/MakeYourPlaceStandOutViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MakeYourPlaceStandOutViewController.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 24/01/2025.
+//
+
+import UIKit
+
+class MakeYourPlaceStandOutViewController: UIViewController {
+    
+    
+    @IBOutlet weak var saveAndExitButtonLabel: UIButton!
+    @IBOutlet weak var questionsButtonLabel: UIButton!
+    @IBOutlet weak var backButtonLabel: UIButton!
+    @IBOutlet weak var nextButtonLabel: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        setupCornerRadius()
+        setupBorderWidth()
+        setupButtonUnderline()
+    }
+    
+    func setupCornerRadius() {
+        saveAndExitButtonLabel.layer.cornerRadius = saveAndExitButtonLabel.frame.height / 2
+        questionsButtonLabel.layer.cornerRadius = questionsButtonLabel.frame.height / 2
+        nextButtonLabel.layer.cornerRadius = nextButtonLabel.frame.height / 5
+    }
+    
+    func setupBorderWidth() {
+        saveAndExitButtonLabel.layer.borderWidth = 0.3
+        questionsButtonLabel.layer.borderWidth = 0.3
+    }
+    
+    func setupButtonUnderline() {
+        guard let buttonText = backButtonLabel.titleLabel?.text else {return}
+        
+        // Create an NSMutableAttributedString from the button's title text
+        let attributedString = NSMutableAttributedString(string: buttonText)
+        
+        // Add underline attribute to the full text
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: buttonText.count))
+        
+        // Set the attributed text to the button's title
+        backButtonLabel.setAttributedTitle(attributedString, for: .normal)
+        
+        // Optional: Change the text color (you can set any color you prefer)
+        backButtonLabel.setTitleColor(UIColor.black, for: .normal)
+    }
+
+}
