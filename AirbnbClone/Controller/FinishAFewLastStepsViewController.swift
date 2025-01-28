c350b9997e96c8f0b129e5d8c634081e8f6fdccb
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-28 12:57:03 +0500

    Fnish a few steps screen work done

---
+++ AirbnbClone/Controller/FinishAFewLastStepsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinishAFewLastStepsViewController.swift
+//  AirbnbClone
+//
+//  Created by Elexoft on 28/01/2025.
+//
+
+import UIKit
+
+class FinishAFewLastStepsViewController: UIViewController {
+
+    @IBOutlet weak var listingImageView: UIImageView!
+    @IBOutlet weak var listingDiscription: UILabel!
+    @IBOutlet weak var listinglocationName: UILabel!
+    @IBOutlet weak var finishupButtonLabel: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupCornerRadius()
+    }
+
+    func setupCornerRadius() {
+        listingImageView.layer.cornerRadius = listingImageView.frame.height / 5
+        finishupButtonLabel.layer.cornerRadius = finishupButtonLabel.frame.height / 5
+    }
+    
+}
