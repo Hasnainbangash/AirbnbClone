7cb9995e4e291c5d0d8ba95a392e1a76ca471ff3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-31 11:13:56 +0500

    Searching and shwoing it on the ap work done

--- AirbnbClone/Controller/WhereYourPlaceLocatedViewController.swift
+++ AirbnbClone/Controller/WhereYourPlaceLocatedViewController.swift
@@ -20,6 +20,18 @@ class WhereYourPlaceLocatedViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    private let searchResultsTableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.isHidden = true
+        table.layer.cornerRadius = 10
+        table.layer.masksToBounds = true
+        return table
+    }()
+    
+    private let searchCompleter = MKLocalSearchCompleter()
+    private var searchResults: [MKLocalSearchCompletion] = []
+    
     let locationManager = CLLocationManager()
     var currentLocation: CLLocationCoordinate2D?
     
@@ -32,10 +44,29 @@ class WhereYourPlaceLocatedViewController: UIViewController {
         
         enterYourAddressTextField.delegate = self
         
+        searchCompleter.delegate = self
+        
+        setupSearchResultsTableView()
         setupLocationManager()
         setupCornerRadius()
         setupBorderWidth()
         setupButtonUnderline()
+    }
+    
+    private func setupSearchResultsTableView() {
+        view.addSubview(searchResultsTableView)
+        
+        // Setup table view constraints
+        NSLayoutConstraint.activate([
+            searchResultsTableView.topAnchor.constraint(equalTo: enterYourAddressView.bottomAnchor, constant: 8),
+            searchResultsTableView.leadingAnchor.constraint(equalTo: enterYourAddressView.leadingAnchor),
+            searchResultsTableView.trailingAnchor.constraint(equalTo: enterYourAddressView.trailingAnchor),
+            searchResultsTableView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        searchResultsTableView.delegate = self
+        searchResultsTableView.dataSource = self
+        searchResultsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "LocationCell")
     }
     
     func setupLocationManager() {
@@ -93,20 +124,75 @@ extension WhereYourPlaceLocatedViewController: UIViewControllerTransitioningDele
     
 }
 
+// MARK: - UITextFieldDelegate
 extension WhereYourPlaceLocatedViewController: UITextFieldDelegate {
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // Perform the search when the user presses "Enter"
-        if let query = textField.text, !query.isEmpty {
-            performSearch(query: query)
-        }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? string
+        searchCompleter.queryFragment = newText
         
-        // Dismiss the keyboard
-        textField.resignFirstResponder()
-        
+        searchResultsTableView.isHidden = newText.isEmpty
         return true
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        searchResultsTableView.isHidden = true
+        
+        if let query = textField.text, !query.isEmpty {
+            performSearch(query: query)
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty {
+            searchResultsTableView.isHidden = false
+        }
+    }
+    
+}
+
+extension WhereYourPlaceLocatedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        let searchResult = searchResults[indexPath.row]
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = searchResult.title
+        config.secondaryText = searchResult.subtitle
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let completion = searchResults[indexPath.row]
+        let searchRequest = MKLocalSearch.Request(completion: completion)
+        
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { [weak self] response, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("Selection error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let response = response,
+                  let firstItem = response.mapItems.first else { return }
+            
+            self.enterYourAddressTextField.text = completion.title
+            self.searchResultsTableView.isHidden = true
+            self.updateMap(with: firstItem)
+        }
+    }
 }
 
 extension WhereYourPlaceLocatedViewController: CLLocationManagerDelegate {
@@ -126,7 +212,14 @@ extension WhereYourPlaceLocatedViewController: CLLocationManagerDelegate {
         guard let userLocation = locations.last else { return }
         
         locationManager.stopUpdatingLocation()
-        self.currentLocation = userLocation.coordinate
+        currentLocation = userLocation.coordinate
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = userLocation.coordinate
+        pin.title = "Current Location"
+        
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(pin)
         
         let region = MKCoordinateRegion(
             center: userLocation.coordinate,
@@ -135,20 +228,10 @@ extension WhereYourPlaceLocatedViewController: CLLocationManagerDelegate {
         )
         
         mapView.setRegion(region, animated: true)
-        
-        mapView.removeAnnotations(mapView.annotations)
-        
-        let pin = MKPointAnnotation()
-        pin.coordinate = userLocation.coordinate
-        pin.title = "Current Location"
-        mapView.addAnnotation(pin)
-        
     }
     
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
-        print("-----------------------------------------")
-        print(error)
-        print("-----------------------------------------")
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager error: \(error.localizedDescription)")
     }
     
 }
@@ -188,6 +271,32 @@ extension WhereYourPlaceLocatedViewController: MKLocalSearchCompleterDelegate {
                 
             }
         }
+    }
+       
+    private func updateMap(with mapItem: MKMapItem) {
+        let pin = MKPointAnnotation()
+        pin.title = mapItem.name
+        pin.coordinate = mapItem.placemark.coordinate
+        
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(pin)
+        
+        let region = MKCoordinateRegion(
+            center: mapItem.placemark.coordinate,
+            latitudinalMeters: 5000,
+            longitudinalMeters: 5000
+        )
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        searchResultsTableView.reloadData()
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        print("Search completer error: \(error.localizedDescription)")
     }
     
 }
