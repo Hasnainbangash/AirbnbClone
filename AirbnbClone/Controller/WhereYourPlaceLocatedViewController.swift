284e2da6370dd1948c6e959831a22a0ad2280086
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-31 10:45:27 +0500

    Map kit is implemenetd in where your place located screen

--- AirbnbClone/Controller/WhereYourPlaceLocatedViewController.swift
+++ AirbnbClone/Controller/WhereYourPlaceLocatedViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class WhereYourPlaceLocatedViewController: UIViewController {
 
@@ -15,6 +16,12 @@ class WhereYourPlaceLocatedViewController: UIViewController {
     @IBOutlet weak var nextButtonLabel: UIButton!
     
     @IBOutlet weak var enterYourAddressView: UIView!
+    @IBOutlet weak var enterYourAddressTextField: UITextField!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocationCoordinate2D?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +30,18 @@ class WhereYourPlaceLocatedViewController: UIViewController {
         
         self.transitioningDelegate = self
         
+        enterYourAddressTextField.delegate = self
+        
+        setupLocationManager()
         setupCornerRadius()
         setupBorderWidth()
         setupButtonUnderline()
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     func setupCornerRadius() {
@@ -57,6 +73,12 @@ class WhereYourPlaceLocatedViewController: UIViewController {
         backButtonLabel.setTitleColor(UIColor.black, for: .normal)
     }
     
+    @IBAction func currentLocationButtonPressed(_ sender: UIButton) {
+        print("Location button pressed")
+        locationManager.requestLocation()
+        locationManager.startUpdatingLocation()
+    }
+    
     @IBAction func backButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -67,6 +89,105 @@ extension WhereYourPlaceLocatedViewController: UIViewControllerTransitioningDele
     
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return SlideOutToLeftAnimator() // Use our custom animator for sliding out to the left
+    }
+    
+}
+
+extension WhereYourPlaceLocatedViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Perform the search when the user presses "Enter"
+        if let query = textField.text, !query.isEmpty {
+            performSearch(query: query)
+        }
+        
+        // Dismiss the keyboard
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+}
+
+extension WhereYourPlaceLocatedViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        case .denied, .restricted:
+            print("Location permission denied.")
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let userLocation = locations.last else { return }
+        
+        locationManager.stopUpdatingLocation()
+        self.currentLocation = userLocation.coordinate
+        
+        let region = MKCoordinateRegion(
+            center: userLocation.coordinate,
+            latitudinalMeters: 5000,
+            longitudinalMeters: 5000
+        )
+        
+        mapView.setRegion(region, animated: true)
+        
+        mapView.removeAnnotations(mapView.annotations)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = userLocation.coordinate
+        pin.title = "Current Location"
+        mapView.addAnnotation(pin)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("-----------------------------------------")
+        print(error)
+        print("-----------------------------------------")
+    }
+    
+}
+
+extension WhereYourPlaceLocatedViewController: MKLocalSearchCompleterDelegate {
+    
+    func performSearch(query: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = query
+        
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            
+            if let e = error {
+                print(e.localizedDescription)
+                return
+            }
+            
+            guard let response = response else { return }
+            
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            
+            if let location = response.mapItems.last {
+                
+                let pin = MKPointAnnotation()
+                pin.title = location.name
+                pin.coordinate = location.placemark.coordinate
+                self.mapView.addAnnotation(pin)
+                
+                let region = MKCoordinateRegion(
+                    center: location.placemark.coordinate,
+                    latitudinalMeters: 5000,
+                    longitudinalMeters: 5000
+                )
+                
+                self.mapView.setRegion(region, animated: true)
+                
+            }
+        }
     }
     
 }
